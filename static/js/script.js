0406516c234781739befa8965df48d08b89ace76
Jônatas G <jonatasgeek@gmail.com>
2022-07-31 23:47:24 -0300

    Update js script and set translation

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+const formResponse = document.getElementById('form-response');
+const loadingEl = document.getElementById('loading');
+
+$('#form').ajaxForm(function(result) {
+    formResponse.innerHTML = result;
+    $('#main-table').DataTable({
+        order: [[7, 'desc']],
+        language: {
+            "url": "//cdn.datatables.net/plug-ins/1.12.1/i18n/pt-BR.json"
+        }
+    });
+    $('#fails-table').DataTable();
+    formResponse.scrollIntoView();
+});
